72a9a7e2304ef80a24a150c081b7e482bc919056
Tamir Vered <takuyaonrain@hotmail.com>
2016-07-10 21:39:06 +0300

    - Added ArgsData's information required for smg to generate the code (inputs/outputs/configurations/etc...).

--- smg/smg/ArgumentParsing/ArgsData.cs
+++ smg/smg/ArgumentParsing/ArgsData.cs
@@ -1,4 +1,5 @@
-﻿using smg.ArgumentParsing.Attributes;
+﻿using System.Reflection;
+using smg.ArgumentParsing.Attributes;
 
 namespace smg.ArgumentParsing
 {
@@ -12,5 +13,29 @@ namespace smg.ArgumentParsing
         /// </summary>
         [BooleanArg("/h", "-h", "/help", "-help", "/?", "-?")]
         public bool Help { get; private set; }
+
+        /// <summary>
+        /// Indicates that the generator should generate a DLL instead of .cs files.
+        /// </summary>
+        [BooleanArg("/a", "/assembly", "/d", "/dll")]
+        public bool GenerateAssembly { get; private set; }
+
+        /// <summary>
+        /// Specifies the path to the <see cref="Assembly"/> which contains the class for the code to be generated according to.
+        /// </summary>
+        [IndexedArg(0)]
+        public string SourceAssemblyPath { get; private set; }
+
+        /// <summary>
+        /// Specifies the class which will be used to generate the states according to.
+        /// </summary>
+        [IndexedArg(1)]
+        public string StateClassName { get; private set; }
+
+        /// <summary>
+        /// Specifies the path for the generated .cs/DLLs to be saved to.
+        /// </summary>
+        [IndexedArg(2)]
+        public string OutputPath { get; private set; }
     }
 }
