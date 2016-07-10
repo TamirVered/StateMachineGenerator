f33fd75df59e4e45f44701db39587b28f997d945
Tamir Vered <takuyaonrain@hotmail.com>
2016-07-10 21:54:51 +0300

    - Added help.

--- smg/smg/StateMachineGenerator.cs
+++ smg/smg/StateMachineGenerator.cs
@@ -9,6 +9,21 @@ namespace smg
 {
     class StateMachineGenerator
     {
+        private static readonly string HELP = "Made by Tamir Vered 2016" + Environment.NewLine
+            + Environment.NewLine
+            + "This utility allows generation of safe state machine wrappers according to attributed compiled class with many possible usages such as: " + Environment.NewLine
+            + " - Safe factories." + Environment.NewLine
+            + " - Unit testing tools." + Environment.NewLine
+            + " - Safe stateful object handling." + Environment.NewLine
+            + Environment.NewLine
+            + "Usage:" + Environment.NewLine
+            + "smg.exe source_assembly source_type_name destination_folder [/A | /D]" + Environment.NewLine
+            + Environment.NewLine
+            + "  /A | /D     Set output to compiled DLL instead of source files." + Environment.NewLine
+            + Environment.NewLine
+            + "For more information visit the project's github on https://github.com/TamirVered/StateMachineGenerator" + Environment.NewLine;
+
+
         static void Main(string[] args)
         {
             ArgsData argsData;
@@ -17,11 +32,16 @@ namespace smg
                 DisplayHelp();
                 return;
             }
+            if (argsData.Help)
+            {
+                DisplayHelp();
+                return;
+            }
         }
 
         private static void DisplayHelp()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(HELP);
         }
     }
 }
